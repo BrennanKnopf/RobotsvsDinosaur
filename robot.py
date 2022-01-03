6f11bb9a48c4e8e8b3763a92c4d190fd8fd7e33c
BrennanKnopf <brennanknopf@gmail.com>
2022-01-03 13:19:08 -0600

    realized i put the wrong lable in my dinosaur attack method. Changed type to kind.

--- robot.py
+++ robot.py
@@ -13,17 +13,17 @@ class Robot:
             attack_choice = int(input(f'Choose what robot to attack with: (1) {self.weapon_choice[0]}, (2) {self.weapon_choice[1]}, (3) {self.weapon_choice[2]}.'))
         for attack in attack_choice:
             if attack_choice == 1:
-               print(f'{self.name} attacked {dinosaur_attacked.type} with {self.weapon_choice[0]}')
+               print(f'{self.name} attacked {dinosaur_attacked.kind} with {self.weapon_choice[0]}')
             elif attack_choice == 2:
-                print(f'{self.name} attacked {dinosaur_attacked.type} with {self.weapon_choice[1]}')
+                print(f'{self.name} attacked {dinosaur_attacked.kind} with {self.weapon_choice[1]}')
             elif attack_choice == 3:
-                print(f'{self.name} attacked {dinosaur_attacked.type} with {self.weapon_choice[3]} ')
+                print(f'{self.name} attacked {dinosaur_attacked.kind} with {self.weapon_choice[3]} ')
                 
 
         self.energy_level -= 10
         dinosaur_attacked -= self.weapon.attack_power
         print(f'{self.name} energy level is now {self.energy_level}') 
-        print(f'{dinosaur_attacked.type} health is now {dinosaur_attacked.health}')       
+        print(f'{dinosaur_attacked.kind} health is now {dinosaur_attacked.health}')       
 
 
 

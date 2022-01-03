6ae647eb51798a364fd7fe26a12bb6b92eaac571
BrennanKnopf <brennanknopf@gmail.com>
2022-01-03 12:22:07 -0600

    Established the herd class. Created the dinosaurs themselves.

--- herd.py
+++ herd.py
@@ -1,6 +1,13 @@
+from dinosaur import Dinosaur
+#here i will create the dinosaurs to put in the herd
 class Herd:
     def __init__(self):
         self.storage = []
+        self.create_herd()
     
     def create_herd(self):
-        pass
+        dino_one = Dinosaur('Triceratops', 20)
+        dino_two = Dinosaur('Ankylosaurus', 25)
+        dino_three = Dinosaur('Velociraptor', 15)
+    
+    

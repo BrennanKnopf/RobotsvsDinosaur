f68721eef272e03c50ffb6102653c3d12f9daf53
BrennanKnopf <brennanknopf@gmail.com>
2022-01-04 15:37:39 -0600

    Changed robot energy level variable to power level.

--- robot.py
+++ robot.py
@@ -4,19 +4,19 @@ class Robot:
     def __init__(self, name, weapon):
         self.name = name
         self.health = 100
-        self.energy_level = 100
+        self.power_level = 100
         self.weapon = weapon
        
         
     def attack_dinosaur(self, dinosaur_attacked):
-        while self.energy_level > 10:
+        while self.power_level > 10:
             print(f'{self.name} attacked {dinosaur_attacked.kind} with {self.weapon.name}')
             break
            
 
-        self.energy_level -= 10
+        self.power_level -= 10
         dinosaur_attacked.health -= self.weapon.attack_power
-        print(f'{self.name} energy level is now {self.energy_level}') 
+        print(f'{self.name} energy level is now {self.power_level}') 
         print(f'{dinosaur_attacked.kind} health is now {dinosaur_attacked.health}')       
 
 
